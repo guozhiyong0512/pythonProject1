652a5cb7cdccdc772d3dce5788575ca3ce4b1580
guozhiyong <guozhiyong@zhidianlife.com>
2021-02-03 13:59:12 +0800

    企业微信接口第一次作业

--- test_requests/test_contact.py
+++ test_requests/test_contact.py
@@ -3,7 +3,7 @@ import requests
 
 def test_contact():
     res = requests.get(
-        'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=wwa50a825b2be98d47&corpsecret=IaSqhhALhrwKvaSsN9kepqB_vQllQWUQzZ4kgPBALAI')
+        'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=wwa50a825b2be98d47&corpsecret=IaSqhhALhrwKvaSsN9kepjPtD_ltNS8DCbxqw9FbAk0')
     print(res.json()['access_token'])
     token = res.json()['access_token']
     return token
@@ -20,7 +20,7 @@ def test_update():
     url_update = f'https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token={token}'
     data = {
         "userid": "123",
-        "name": "李四"
+        "name": "老白"
     }
     res2 = requests.post(url=url_update, json=data)
     print(res2.json())
@@ -40,7 +40,8 @@ def test_add():
     data = {
         "userid": "123www",
         "name": "李四",
-        "mobile": "+86 13800000000"
+        "mobile": "+86 13800000011",
+        "department": [1]
     }
     res2 = requests.post(url=url_update, json=data)
     print(res2.json())
